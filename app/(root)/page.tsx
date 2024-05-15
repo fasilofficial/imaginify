d48fea6adf4a721284f6e62701caca7d287f24eb
fasilofficial <muhammedfasiloffcial@gmail.com>
2024-05-15 20:08:51 +0530

    Implement Clerk auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <p>Home</p>
+    </div>
+  );
 };
 
 export default Home;
