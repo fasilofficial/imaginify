cc7bc7b0b92e826618b4801545a088c2276407c1
fasilofficial <muhammedfasiloffcial@gmail.com>
2024-05-16 09:54:26 +0530

    Modify webhooks/clerk

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -65,7 +65,7 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username!,
+      username: username || "",
       firstName: first_name || "",
       lastName: last_name || "",
       photo: image_url,
@@ -92,7 +92,7 @@ export async function POST(req: Request) {
     const user = {
       firstName: first_name || "",
       lastName: last_name || "",
-      username: username!,
+      username: username || '',
       photo: image_url,
     };
 
