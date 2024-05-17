c6e28228a29c3f2aa7649fc65a8c885985951026
fasilofficial <muhammedfasiloffcial@gmail.com>
2024-05-17 22:48:29 +0530

    Fix build errors related to Clerk

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhooks/clerk",
+  "/api/webhooks/stripe",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
