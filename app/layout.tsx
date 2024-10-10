ca1208139f1c28e1eed022d484cae5415ca242b3
Muhammed F <muhammedfasilofficial@gmail.com>
2024-10-10 09:04:18 +0530

    Add Google Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const imbPlex = IBM_Plex_Sans({
+const ibmPlex = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-ibm-plex",
@@ -24,17 +25,20 @@ export default function RootLayout({
   return (
     <ClerkProvider appearance={{ variables: { colorPrimary: "#624CF5" } }}>
       <html lang="en">
-        <body className={cn("font-ibmPlex antialiased", imbPlex.variable)}>
-          <!-- Google tag (gtag.js) -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-BE1GL6Q70E"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', 'G-BE1GL6Q70E');
-          </script>
+        <body className={cn("font-ibmPlex antialiased", ibmPlex.variable)}>
           {children}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-BE1GL6Q70E"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BE1GL6Q70E');
+            `}
+          </Script>
           <SpeedInsights />
         </body>
       </html>
